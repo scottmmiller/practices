c2f6713f07c86137e4fa6f63879fd7eb7f92914b
skootdiggity <smmiller.work@gmail.com>
2015-01-16 18:21:23 -0700

    initial push

---
+++ callbackPractice_Practice.js
@@ -0,0 +1,202 @@
+/* In this repo your job is to write functions to make each function call work properly.
+Below is a sample problem 
+  //code here for sayHi
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay);
+   });
+   
+and what you should write is the sayHi function that makes the code above work, 
+    
+    
+   var sayHi = function(str, cb){
+    cb(str);
+   }
+   sayHi('Hi Katie', function(thingToSay){
+      alert(thingToSay); //should alert ('Hi Katie')'
+   });
+    
+    
+*/
+
+
+
+  //Code Here for first
+var first = function (arr, callback) {
+  callback(arr[0]);
+};
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+first(names, function(firstName){
+  console.log('The first name in names is ', firstName)
+});
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+  //Code Here for last
+var last = function (arr, callback) {
+  callback(arr[arr.length -1]);
+};
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+last(names, function(lastName){
+  console.log('The last name in names is ', lastName);
+});
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+
+  //Code Here for multiply
+var multiply = function (num1, num2, callback) {
+  callback(num1 * num2);
+};
+
+
+multiply(4, 3, function(answer){
+  console.log('The answer is ', answer); //should console.log 12
+})
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+  //Code Here for contains
+var contains = function (arr, name, callback) {
+  callback(name, arr.indexOf(name) !== -1);
+};
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+contains(names, 'Colt', function(name, result){
+  if(result === true){
+    console.log(name + ' is in the array');
+  } else {
+    console.log(name + ' is not in the array');
+  }
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for uniq
+var uniq = function (arr, callback) {
+  var uniqArr = [];
+  for (var i in arr) {
+    if (uniqArr.indexOf(arr[i]) === -1) {
+      uniqArr.push(arr[i]);
+    };
+  };
+  callback(uniqArr);
+};
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+
+***CLASS SOLUTION
+
+var uniq = function (arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if (i !== arr.indexOf(arr[i])){
+      arr.splice(i, 1);
+      i--;
+    };
+  };
+  callback(arr);
+};
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+uniq(names, function(uniqArr){
+  console.log('The new names array with all the duplicate items removed is ', uniqArr);
+});
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+    //Code Here for each
+var each = function (arr, callback) {
+  for (var i = 0; i < arr.length; i++) {
+  callback(arr[i], i);
+  };
+};
+
+
+var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
+each(names, function(item, indice){
+  console.log('The item in the ' + indice + ' position is ' + item)
+});
+
+
+
+
+
+/* NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM ---- NEXT PROBLEM */
+
+
+
+
+
+ //code here for getUserById
+var getUserById = function (arr, id, callback) {
+  for (var i = 0; i < arr.length; i++) {
+    if(id === arr[i].id){
+    callback(id, arr[i]);
+    };
+  };
+};
+
+
+var users = [
+  {
+    id: '12d',
+    email: 'tyler@gmail.com',
+    name: 'Tyler',
+    address: '167 East 500 North'
+  },
+  {
+    id: '15a',
+    email: 'cahlan@gmail.com',
+    name: 'Cahlan',
+    address: '135 East 320 North'
+  },
+  {
+    id: '16t',
+    email: 'ryan@gmail.com',
+    name: 'Ryan',
+    address: '192 East 32 North'
+  },
+];
+
+getUserById(users, '16t', function(id, user){
+  console.log('The user with the id ' + id + ' has the email of ' + user.email + ' the name of ' + user.name + ' and the address of ' + user.address); 
+});
